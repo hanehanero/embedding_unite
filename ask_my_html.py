e3ed7c94f57d5531f752841c90d10337996415ff
hanehanero <hanehanero@gmail.com>
2023-10-23 17:50:41 +0900

    Add Embedding Gabu

---
+++ ask_my_html.py
@@ -0,0 +1,209 @@
+# pip install pycryptodome
+from glob import glob
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
+
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+QDRANT_PATH = "./local_qdrant"
+COLLECTION_NAME = "my_collection_2"
+
+
+def init_page():
+    st.set_page_config(
+        page_title="Ask My HTML(s)",
+        page_icon="🤗"
+    )
+    st.sidebar.title("Nav")
+    st.session_state.costs = []
+
+
+def select_model():
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-3.5-16k", "GPT-4"))
+    if model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo"
+    elif model == "GPT-3.5-16k":
+        st.session_state.model_name = "gpt-3.5-turbo-16k"
+    else:
+        st.session_state.model_name = "gpt-4"
+    
+    # 300: 本文以外の指示のトークン数 (以下同じ)
+    st.session_state.max_token = OpenAI.modelname_to_contextsize(st.session_state.model_name) - 300
+    return ChatOpenAI(temperature=0, model_name=st.session_state.model_name)
+
+
+def validate_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def get_content(url):
+    try:
+        with st.spinner("Fetching Content ..."):
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # fetch text from main (change the below code to filter page)
+            if soup.main:
+                return soup.main.get_text()
+            elif soup.article:
+                return soup.article.get_text()
+            else:
+                return soup.body.get_text()
+    except:
+        st.write('something wrong')
+        return None
+
+
+def get_html_text():
+    url = st.text_input("URL: ", key="input")
+    is_valid_url = validate_url(url)
+    if not is_valid_url:
+        st.write('Please input valid url')
+        return None
+    else:
+        content = get_content(url)
+    if content:
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name="text-embedding-ada-002",
+            # 適切な chunk size は対象によって変わるため調整が必要
+            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
+            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
+            chunk_size=500,
+            chunk_overlap=0,
+        )
+        return text_splitter.split_text(content)
+    else:
+        return None
+
+
+def load_qdrant():
+    client = QdrantClient(path=QDRANT_PATH)
+
+    # すべてのコレクション名を取得
+    collections = client.get_collections().collections
+    collection_names = [collection.name for collection in collections]
+
+    # コレクションが存在しなければ作成
+    if COLLECTION_NAME not in collection_names:
+        # コレクションが存在しない場合、新しく作成します
+        client.create_collection(
+            collection_name=COLLECTION_NAME,
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+        )
+        print('collection created')
+
+    return Qdrant(
+        client=client,
+        collection_name=COLLECTION_NAME, 
+        embeddings=OpenAIEmbeddings()
+    )
+
+
+def build_vector_store(pdf_text):
+    qdrant = load_qdrant()
+    qdrant.add_texts(pdf_text)
+
+    # 以下のようにもできる。この場合は毎回ベクトルDBが初期化される
+    # LangChain の Document Loader を利用した場合は `from_documents` にする
+    # Qdrant.from_texts(
+    #     pdf_text,
+    #     OpenAIEmbeddings(),
+    #     path="./local_qdrant",
+    #     collection_name="my_documents",
+    # )
+
+
+def build_qa_model(llm):
+    qdrant = load_qdrant()
+    retriever = qdrant.as_retriever(
+        # "mmr",  "similarity_score_threshold" などもある
+        search_type="similarity",
+        # 文書を何個取得するか (default: 4)
+        search_kwargs={"k":10}
+    )
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff", 
+        retriever=retriever,
+        return_source_documents=True,
+        verbose=True
+    )
+
+
+def page_read_html_and_build_vector_db():
+    st.title("Read HTML")
+    container = st.container()
+    with container:
+        html_text = get_html_text()
+        if html_text:
+            with st.spinner("Loading HTML ..."):
+                build_vector_store(html_text)
+
+
+def ask(qa, query):
+    with get_openai_callback() as cb:
+        # query / result / source_documents
+        answer = qa(query)
+
+    return answer, cb.total_cost
+
+
+def page_ask_my_pdf():
+    st.title("Ask My HTML(s)")
+
+    llm = select_model()
+    container = st.container()
+    response_container = st.container()
+
+    with container:
+        query = st.text_input("Query: ", key="input")
+        if not query:
+            answer = None
+        else:
+            qa = build_qa_model(llm)
+            if qa:
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = ask(qa, query)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+
+        if answer:
+            with response_container:
+                st.markdown("## Answer")
+                st.write(answer)
+
+
+def main():
+    init_page()
+
+    selection = st.sidebar.radio("Go to", ["Read HTML", "Ask My HTML(s)"])
+    if selection == "Read HTML":
+        page_read_html_and_build_vector_db()
+    elif selection == "Ask My HTML(s)":
+        page_ask_my_pdf()
+
+    costs = st.session_state.get('costs', [])
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
+    for cost in costs:
+        st.sidebar.markdown(f"- ${cost:.5f}")
+
+
+if __name__ == '__main__':
+    main()
